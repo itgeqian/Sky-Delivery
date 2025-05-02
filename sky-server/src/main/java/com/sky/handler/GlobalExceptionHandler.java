e3b6e7f5d8bd1c3eff9a378ed0ca9cd9edbda135
itgeqian <2933909350@qq.com>
2025-05-02 16:23:46 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +27,26 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+    /**
+     * 异常处理器方法，用于捕获并处理 SQL 完整性约束违规异常
+     *
+     * @param ex SQL完整性约束违规异常
+     * @return Result对象，包含异常处理的结果
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")) {
+            // 处理重复数据异常，例如用户名已存在的情况
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg = username+ MessageConstant.ALREADY_EXIST;
 
-}
+            return Result.error(msg);
+            //或者直接 return Result.error("用户名已存在");
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+     }
+  }
+
